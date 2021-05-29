537c208418e502f815e5478b284f5cf36277b5bc
vinopalladino <79374128+vinopalladino@users.noreply.github.com>
2021-05-29 13:28:54 -0700

    C193finalproject

--- sports_credit.js
+++ sports_credit.js
@@ -1,17 +1,7 @@
 "use strict";
 
 /*
-   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
-   Tutorial 13
-   Review Assignment
 
-   Credit Card Form Script
-   
-   Author: profhamachi
-   Date:   04/29/2021
-   
-   Filename: co_credit.js
-   
    Function List
    =============
    
@@ -45,22 +35,24 @@
 */
 
 window.addEventListener("load", function() {
-    // Retrieve the field/value pairs from the URL
-    var orderData = location.search.slice(1);
-    orderData = orderData.replace(/\+/g," ");
-    orderData = decodeURIComponent(orderData);
-    var orderFields = orderData.split(/[&=]/g);
+   // Retrieve the field/value pairs from the URL
+   var orderData = location.search.slice(1);
+   orderData = orderData.replace(/\+/g, " ");
+   orderData = decodeURIComponent(orderData);
+   var orderFields = orderData.split(/[&=]/g);
 
-    // Write the field values to the order form
-   document.forms.order.elements.modelName.value = orderFields[3];
-   document.forms.order.elements.modelQty.value = orderFields[5];
-   document.forms.order.elements.orderCost.value = orderFields[7];
-   document.forms.order.elements.shippingType.value = orderFields[9];
-   document.forms.order.elements.shippingCost.value = orderFields[13];
-   document.forms.order.elements.subTotal.value = orderFields[15];
-   document.forms.order.elements.salesTax.value = orderFields[17];
-   document.forms.order.elements.cartTotal.value = orderFields[19];
-   });
+   // Write the field values to the order form
+   document.forms.order.elements.
+   document.forms.order.elements.
+   document.forms.order.elements.
+   document.forms.order.elements.
+   document.forms.order.elements.
+   document.forms.order.elements.
+   document.forms.order.elements.
+   document.forms.order.elements.
+   document.forms.order.elements.
+   document.forms.order.elements.
+});
 
 window.addEventListener("load", function() {
    document.getElementById("subButton").onclick = runSubmit;
@@ -80,76 +72,48 @@ function runSubmit() {
 
 function validateDate() {
    var cardDate = document.getElementById("expDate");
-   if (cardDate.validity.valueMissing) {
-      cardDate.setCustomValidity("Enter the expiration date");
-   } else if (/^(0[1-9]|1[0-2])\/20[12]\d$/.test(cardDate.value) --- false) {
-      cardDate.setCustomValidity("Enter a valid expiration date")
-   } else {
-      cardDate.setCustomValidity("");
-   }
+   if (cardDate.validity.valueMissing) {cardDate.setCustomValidity("Enter the expiration date.");}
+   else if (/^(0[1-9]|1[0-2])\/20[12]\d$/.test(cardDate.value) === false) {cardDate.setCustomValidity("Enter valid expiration date");}
+   else {cardDate.setCustomValidity("");}
 }
-
-
-
-
-
-
-
-
-
-/* Functions already provided in the file */
 
 function validateName() {
    var cardName = document.getElementById("cardHolder");
-   if (cardName.validity.valueMissing) {
-      cardName.setCustomValidity("Enter the card holder");
-   } else {
-      cardName.setCustomValidity("");
-   }
+   if (cardName.validity.valueMissing) {cardName.setCustomValidity("Enter name of card holder");}
+   else {cardName.setCustomValidity("");}
 }
 
 
 function validateCredit() {
    var creditCard = document.forms.credit.elements.company[0];
-   if (creditCard.validity.valueMissing) {
-      creditCard.setCustomValidity("Select your credit card");
-   } else {
-      creditCard.setCustomValidity("");
-   }
+   if (credidCard.validity.valueMissing) {creditCard.setCustomValidity("Select your credit card company");}
+   else {creditCard.setCustomValidity("");}
 }
 
 function validateNumber() {
    var cardNumber = document.getElementById("cardNumber");
-   if (cardNumber.validity.valueMissing) {
-      cardNumber.setCustomValidity("Enter your card number");
-   } else if (cardNumber.validity.patternMismatch) {
-      cardNumber.setCustomValidity("Enter a valid card number");
-   } else if (luhn(cardNumber.value) === false) {
-      cardNumber.setCustomValidity("Enter a legitimate card number");
-   } else {
-      cardNumber.setCustomValidity("");
-   }
+   if (cardNumber.validity.valueMissing) {cardNumber.setCustomValidity("Enter your card number");}
+   else if (cardNumber.validity.patternMismatch) {cardNumber.setCustomValidity("Enter a valid card number");}
+   else if (luhn(cardNumber.value) === false) {cardNumber.setCustomValidity("Enter a legitimate card number");}
+   else {cardNumber.setCustomValidity("");}
 }
 
 function validateCVC() {
    var cardCVC = document.getElementById("cvc");
-   var creditCard = document.querySelector('input[name="company"]:checked').value;
-   
-  if (cardCVC.validity.valueMissing) {
-    cardCVC.setCustomValidity("Enter your code CVC number");
-   } else if ((creditCard === "amex") && (/^\d{4}$/.test(cardCVC.value) === false)) {
-   cardCVC.setCustomValidity("Enter a 4-digit CVC number");
-  } else if ((creditCard !== "amex") && (/^\d{3}$/.test(cardCVC.value) === false)) {
-   cardCVC.setCustomValidity("Enter a 3-digit CVC number");
-  } else {
-   cardCVC.setCustomValidity("");
-  }
+   vard creditCard = document.querySelector('input[name="company"]:checked').value;
+
+   if (cardCVC.validity.valueMissing) {cardCVC.setCustomValidity("Enter your code CVC number");}
+   else if ((creditCard === "amex") && (/^\d{4}$/.test(cardCVC.value) === false)) {
+      cardCVC.setCustomValidity("Enter a 4-digit CVC number");
+   } else if ((creditCard !== "amex") && (/^\d{3}$/.test(cardCVC.value) === false)) {
+      cardCVC.setCustomValidity("Enter a 3-digit CVC number");
+   } else {cardCVC.setCustomValidity("");}
 }
 
 function sumDigits(numStr) {
    var digitTotal = 0;
    for (var i = 0; i < numStr.length; i++) {
-      digitTotal += parseInt(numStr.charAt(i));
+      digitalTotal += parseInt(numStr.charAt(i));
    }
    return digitTotal;
 }
@@ -157,16 +121,17 @@ function sumDigits(numStr) {
 function luhn(idNum) {
    var string1 = "";
    var string2 = "";
-   
-   // Retrieve the odd-numbered digits
-   for (var i = idNum.length - 1; i >= 0; i-= 2) {
+
+   // Get odd number digits
+   for (var i = idNum.length - 1; i >=0; i -= 2) {
       string1 += idNum.charAt(i);
    }
-   // Retrieve the even-numbered digits and double them
-   for (var i = idNum.length - 2; i >= 0; i-= 2) {
-      string2 += 2*idNum.charAt(i);
+
+   // Get even number digits
+   for (var i = idNum.length - 2; i >=0; i -= 2) {
+      string2 += 2 * idNum.charAt(i);
    }
-   
-   // Return whether the sum of the digits is divisible by 10
+
+   // Determine if the sum of the digits is divisible by 10
    return sumDigits(string1 + string2) % 10 === 0;
 }
--- sports_donors_sort.js
+++ sports_donors_sort.js
@@ -1,165 +1,148 @@
 "use strict";
 
-/*
-   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
-   Tutorial 12
-   Case Problem 4
 
-   Author: profhamachi
-   Date:   04/24/21   
-   Filename: gi_sort.js
+/*
+   Filename: ah_product.js  
    
-   Global Variables:
-   tableData
-      An 2-dimensional array of the data found in the body of the web table
-      
-   dataCategories
-      An array of the column titles found the head of the web table
-      
-   sortIndex
-      The index number of the column by which the web table should be
-      sorted where 0 = 1st column, 1 = 2nd column, etc.
-      
-   sortDirection
-      A value indicating direction of the sorting where a value of 1 sorts 
-      the data in ascending order and a value of -1 sorts the data in descending order
-	
-   
-   
-   Functions List:   
-   defineDataArray()
-      Extracts values from the body of the web table and stores them in the
-      tableData array
-      
-   writeTableData()
-      Writes the sorted data into the table rows and cells       
-      
-   defineColumns()
-      Extracts values form the column heads of the web table and stores 
-      them in the dataCategories array; also sets up the onclick event
-      handlers for the column heads so that the table can be sorted by
-      clicking a table heading cell.
-           
-   columnSort(e)
-      Event handler function that sorts the table data when a column
-      heading is clicked  
-   
-   dataSort2D(a, b)
-      Compare function used to sort numeric and alphabetical data from
-      a 2-dimensional array 
-    
+   Variables:
+   the donors multi-dimension array contains the following data with following column indices
+      donors[0] - donorID
+      donors[1] - first name
+      donors[2] - last name
+      donors[3] - phone number
+      donors[4] - email
+      donors[5] - donation amount
+      donors[6] - donation date   
+*/
+
+
+var donors = 
+[
+   ["donor1", "Mildred", "Moore", "606-555-2928", "mmoore@example.com", 200, "3/13/2021"],
+   ["donor2", "Jodi", "Wells", "859-555-4667", "jwells@example.com", 1000, "3/13/2021"],
+   ["donor3", "Irene", "Berry",  "502-555-2851", "iberry@example.com/mail", 2000, "3/15/2021"],
+   ["donor4", "Guillermo", "Shafer", "859-555-9374", "gshafe@example.com/mail", 200, "3/16/2021"],
+   ["donor5", "Monica", "Gray", "270-555-3249", "mgray@example.com/mail", 200, "3/17/2021"],
+   ["donor6", "Jerold", "Cole", "502-555-9286", "jcole@example.com/mail", 100, "3/20/2021"],
+   ["donor7", "David", "Butler", "859-555-1769", "dbutle@example.com/mail", 200, "3/20/2021"],
+   ["donor8", "Mae", "Tillman", "502-555-7789", "mtillm@example.com/mail", 2000, "3/21/2021"],
+   ["donor9", "Raymond", "Adams", "270-555-1686", "radams@example.com/mail", 200, "3/23/2021"],
+   ["donor10", "James", "Smith", "859-555-8359", "jsmith@example.com/mail", 200, "3/23/2021"],
+   ["donor11", "Frederick", "Turner", "606-555-6493", "fturne@example.com/mail", 100, "3/23/2021"],
+   ["donor12", "Alfred", "Woodson", "502-555-2485", "awoods@example.com/mail", 100, "3/29/2021"],
+   ["donor13", "Matthew", "Hebert", "502-555-1445", "mheber@example.com/mail", 100, "3/29/2021"],
+   ["donor14", "Michelle", "Hatfield", "270-555-5533", "mhatfi@example.com/mail", 100, "4/3/2021"],
+   ["donor15", "Jeffrey", "Harris", "502-555-7281", "jharri@example.com/mail", 100, "4/5/2021"],
+   ["donor16", "Corene", "Reece", "270-555-5783", "creece@example.com/mail", 200, "4/7/2021"],
+   ["donor17", "Francis", "Guzman", "859-555-7487", "fguzma@example.com/mail", 100, "4/11/2021"],
+   ["donor18", "Patricia", "Curtis", "859-555-2443", "pcurti@example.com/mail", 100, "4/13/2021"],
+   ["donor19", "William", "Joyce", "502-555-9262", "wjoyce@example.com/mail", 100, "4/14/2021"],
+   ["donor20", "Margaret", "Parra", "502-555-4388", "mparra@example.com/mail", 200, "4/17/2021"],
+   ["donor21", "Toni", "Bourdon",  "606-555-2757", "tbourd@example.com/mail", 5000, "4/19/2021"],
+   ["donor22", "Joshua", "Voss", "859-555-9684", "jvoss@example.com/mail", 100, "4/19/2021"],
+   ["donor23", "Marilyn", "Sams", "606-555-2458", "msams@example.com/mail", 100, "4/20/2021"],
+   ["donor24", "Donald", "Brown", "502-555-2786", "dbrown@example.com/mail", 100, "4/26/2021"],
+   ["donor25", "Richard", "Grahm", "606-555-7384", "rgrahm@example.com/mail", 500, "4/26/2021"],
+   ["donor26", "Marjorie", "Mendez", "859-555-2683", "mmende@example.com/mail", 500, "4/28/2021"],
+   ["donor27", "Royce", "Natividad", "502-555-8112", "rnativ@example.com/mail", 100, "5/2/2021"],
+   ["donor28", "Robert", "Pelletier", "859-555-6454", "rpelle@example.com/mail", 200, "5/2/2021"],
+   ["donor29", "Mary", "Tso", "502-555-3586", "mtso@example.com/mail", 200, "5/2/2021"],
+   ["donor30", "Michele", "Reed", "502-555-5133", "mreed@example.com/mail", 200, "5/4/2021"],
+   ["donor31", "Tiffany", "Grainger", "859-555-6554", "tgrain@example.com/mail", 200, "5/4/2021"],
+   ["donor32", "Megan", "Sanders", "859-555-3857", "msande@example.com/mail", 100, "5/4/2021"],
+   ["donor33", "Grace", "Montgomery", "606-555-2839", "gmontg@example.com/mail", 200, "5/4/2021"],
+   ["donor34", "Harold", "Cook", "270-555-1134", "hcook@example.com/mail", 1000, "5/5/2021"],
+   ["donor35", "Nicolette", "Howe", "502-555-3462", "nhowe@example.com/mail", 1000, "5/9/2021"],
+   ["donor36", "Todd", "Rhodes", "859-555-8411", "trhode@example.com/mail", 200, "5/11/2021"],
+   ["donor37", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+   ["donor38", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+   ["donor39", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+   ["donor40", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+   ["donor41", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+   ["donor42", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+   ["donor43", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+   ["donor44", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+   ["donor45", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+   ["donor46", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+   ["donor47", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+   ["donor48", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+   ["donor49", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+   ["donor50", "Louis", "Valdez", "502-555-9779", "lvalde@example.com/mail", 100, "5/16/2021"],
+]
+
+/*
+   Author:  
+   Date:
+
+   Filename: ah_report.js
+
+   Functions:
+
+   calcSum(donorAmt)
+      A callback function that adds the current donation amount in the array to the donationTotal variable
+
+   findMajorDonors(donorAmt)
+      A callback function that returns the value true only if the current donation amount in the array
+      is greater than or equal to 1000
+
+   donorSortDescending(a, b)
+      A callback function used to sort the donation amounts from the array in descending order
+
+   writeDonorRow(value)
+      A callback function that writes the HTML code for each table row that provides the contact
+      information for the donor
 
 */
 
-var tableData = [];
-var dataCategories = [];
-var sortIndex = 0;
-var sortDirection = 1;
+/* Calculate the total donation for all donors */
+var donationTotal = 0;
+donors.forEach(calcSum);
 
-window.addEventListener("load", function() {
-    defineDataArray();
-    writeTableData();
-    defineColumns();
-});
+/* Display the summary of total number of donors and total donation */
+var summaryTable = "<table>";
+summaryTable += "<tr><th>Donors</th><td>" + donors.length + "</td></tr>";
+summaryTable += "<tr><th>Total Donations</th><td>$" + donationTotal.toLocaleString() + "</td></tr>" 
+summaryTable += "</table>";
 
-function defineDataArray() {
+document.getElementById("donationSummary").innerHTML = summaryTable;
 
-    var tableRows = document.querySelectorAll("table.sortable tbody tr");
+/* Create an array of donors who contributed $100 or more */
+var majorDonors = donors.filter(findMajorDonors);
 
-    for (var i = 0; i < tableRows.length; i++) {
-        var rowCells = tableRows[i].children;
-        var rowValues = new Array(rowCells.length);
-        for (var j = 0; j < rowCells.length; j++) {
-            rowValues[j] = rowCells[j].textContent;
-        }
-        tableData[i] = rowValues;
-    }
-    tableData.sort(dataSort2D);
+/* Sort the array in descending order of donation */
+majorDonors.sort(donorSortDescending);
+
+/* Create a table of major donors */
+var donorTable = "<table>";
+donorTable += "<caption>Major Donors</caption>";
+donorTable += "<tr><th>Donation</th><th>Donor ID</th><th>Date</th><th>Name</th><th>Phone</th><th>E-mail</th></tr>"
+
+/* Write a separate table row for each donor */
+majorDonors.forEach(writeDonorRow);
+donorTable += "</table>"
+document.getElementById("donorTable").innerHTML = donorTable;
+
+function calcSum(donorAmt) {
+   donationTotal += donorAmt[5];
 }
 
-function writeTableData() {
-    var newTableBody = document.createElement("tbody");
-    for (var i = 0; i < tableData.length; i++) {
-        var tableRow = document.createElement("tr");
-
-        for (var j = 0; j < tableData[i].length; j++) {
-            var tableCell = document.createElement("td");
-            tableCell.textContent = tableData[i][j];
-            tableRow.appendChild(tableCell);
-        }
-        newTableBody.appendChild(tableRow);
-    }
-    var sortTable = document.querySelector("table.sortable");
-    var oldTableBody = sortTable.lastElementChild;
-    sortTable.replaceChild(newTableBody, oldTableBody);
+function findMajorDonors(donorAmt) {
+   return donorAmt[5] >= 100;
 }
 
-function defineColumns() {
-
-    var newSheet = document.createElement("style");
-    document.head.appendChild(newSheet);
-    var columnStyles = document.styleSheets[document.styleSheets.length -1];
-    columnStyles.insertRule(
-        "table.sortable thead tr th {\
-            cursor: pointer;\
-        }",0);
-    columnStyles.insertRule(
-        "table.sortable thead tr th::after {\
-            content: '\\00a0';\
-            font-family: monospace; \
-            margin-left: 5px; \
-        }",1);
-    columnStyles.insertRule(
-        "table.sortable thead tr th:nth-of-type(1)::after {\
-            content: '\\25b2';\
-        }",2);
-
-    var columnHeaders = document.querySelectorAll("table.sortable thead tr th");
-    for (var i = 0; i < columnHeaders.length; i++) {
-        dataCategories[i] = columnHeaders[i].textContent;
-        columnHeaders[i].onclick = columnSort;
-    }
+function donorSortDescending(a, b) {
+   return b[5] - a[5];
 }
 
-function columnSort(e) {
-
-    var columnText = e.target.textContent;
-    var columnIndex = dataCategories.indexOf(columnText);
-
-    if (columnIndex === sortIndex) {
-        sortDirection = -sortDirection;
-    } else {
-        sortIndex = columnIndex;
-    }
-    var columnNumber = columnIndex + 1;
-
-    var columnStyles = document.styleSheets[document.styleSheets.length - 1];
-    columnStyles.deleteRule(2);
-
-    if (sortDirection === 1) {
-        columnStyles.insertRule(
-            "table.sortable thead tr th:nth-of-type(" + columnNumber + ")::after {\
-                content: '\\25b2';\
-            }",2);
-    } else {
-        columnStyles.insertRule(
-            "table.sortable thead tr th:nth-of-type(" + columnNumber + ")::after {\
-                content: '\\25bc';\
-            }", 2);
-    }
-    tableData.sort(dataSort2D);
-    writeTableData();
+function writeDonorRow(value) {
+   donorTable += "<tr>";
+   donorTable += "<td>$" + value[5].toLocaleString() + "</td>";
+   donorTable += "<td>" + value[0] + "</td>";
+   donorTable += "<td>" + value[6] + "</td>";
+   donorTable += "<td>" + value[2] + ", " + value[1] + "</td>";
+   donorTable += "<td>" + value[3] + "</td>";
+   donorTable += "<td>" + value[4] + "</td>";
+   donorTable += "</tr>";
 }
 
-function dataSort2D(a, b) {
-   if (isNaN(parseFloat(a[sortIndex])) === false) {
-      return (a[sortIndex] - b[sortIndex])*sortDirection;
-   } else {
-      var astring = a[sortIndex].toLowerCase();
-      var bstring = b[sortIndex].toLowerCase();
-           
-      if (bstring > astring) return -sortDirection;
-      if (bstring < astring) return sortDirection;
-      return 0;
-   }
-}
+
