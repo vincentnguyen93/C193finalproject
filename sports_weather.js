57e6942eb9b5528a403c14499dc8e910d77f4524
vincentnguyen93 <79374128+vincentnguyen93@users.noreply.github.com>
2021-06-10 16:47:39 -0700

    Create sports_weather.js

---
+++ sports_weather.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+/*
+   Filename:  sports_weather.js
+
+   Author:  Vincent Nguyen
+   Date:    June 9, 2021
+   
+   This is where the weather API will be included at checkout. 
+   In case someone wants to attend a sporting event, they can know what the 
+   weather will be like.
+   
+ */
+
